fab8dd36dc06535f47f3a68e80fc4a279d8ad247
Daniil Borovov <borovovdaniil@gmail.com>
2023-07-19 20:54:10 +0600

    add logo link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,12 +3,15 @@ import './Header.css'
 function Header() {
   return (
     <header className='header'>
-      <div className='header__logo' />
+      <a href='/' className='header__logo'>Главная</a>
       <nav className='header__links'>
         <a href='/signup' className='header__link'>
           Регистрация
         </a>
-        <a href='/signin' className='header__link header__link_type_green-button'>
+        <a
+          href='/signin'
+          className='header__link header__link_type_green-button'
+        >
           Войти
         </a>
       </nav>
