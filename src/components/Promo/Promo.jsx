77fb40df4b13c641b0540466e3b58e66dbe1e14a
Daniil Borovov <borovovdaniil@gmail.com>
2023-07-19 18:45:50 +0600

    fix promo

--- src/components/Promo/Promo.jsx
+++ src/components/Promo/Promo.jsx
@@ -1,18 +1,21 @@
 import './Promo.css'
+import planetImage from '../../images/web-planet.svg'
 
 function Promo() {
   return (
     <section className='promo'>
       <h1 className='promo__title'>
-        Учебный проект студента факультета Веб-разработки.
+        Учебный проект студента факультета Веб&#8209;разработки.
       </h1>
       <p className='promo__subtitle'>
         Листайте ниже, чтобы узнать больше про этот проект и его создателя.
       </p>
-      <a className='promo__link'>Узнать Больше</a>
+      <a className='promo__link' href='/about'>
+        Узнать Больше
+      </a>
       <img
         className='promo__logo'
-        src='../../images/web-planet.svg'
+        src={planetImage}
         alt='Планета из слов WEB'
       />
     </section>
