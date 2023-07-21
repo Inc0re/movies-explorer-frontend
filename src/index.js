2082f0fed23bf5a20aa0829bd770ead40f4484c7
Daniil Borovov <borovovdaniil@gmail.com>
2023-07-21 18:25:09 +0600

    wrap App into BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
@@ -6,7 +7,7 @@ import App from './components/App/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <>
+  <BrowserRouter>
     <App />
-  </>
+  </BrowserRouter>
 );
