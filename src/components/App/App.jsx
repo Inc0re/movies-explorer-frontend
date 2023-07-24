749887516bec512ad3f445fa24f3d3012c7b86ba
Daniil Borovov <superskillbro@gmail.com>
2023-07-25 03:10:16 +0600

    add Profile component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,7 +7,9 @@ import Movies from '../Movies/Movies'
 import NotFound from '../NotFound/NotFound'
 import Register from '../Register/Register'
 import Login from '../Login/Login'
+import Profile from '../Profile/Profile'
 import './App.css'
+import Header from '../Header/Header'
 
 function App() {
   return (
@@ -17,6 +19,15 @@ function App() {
         <Route path='/movies' element={<FullPage children={<Movies />} />} />
         <Route path='/sign-up' element={<Register />} />
         <Route path='/sign-in' element={<Login />} />
+        <Route
+          path='/profile'
+          element={
+            <>
+              <Header />
+              <Profile />
+            </>
+          }
+        />
         <Route path='*' element={<NotFound />} />
       </Routes>
     </div>
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,42 @@
+import './Profile.css'
+
+function Profile() {
+  const user = {
+    name: 'Виталий',
+    email: 'test@ya.ru',
+  }
+  return (
+    <div className='profile'>
+      <h1 className='profile__title'>Привет, {user.name}</h1>
+      <form onSubmit={console.log('submit')} className='profile__form'>
+        <label className='profile__label' htmlFor='name'>
+          Имя
+        </label>
+        <input
+          className='profile__field'
+          type='text'
+          name='name'
+          id='name'
+          placeholder={user.name}
+          required
+        />
+        <div className='profile__split' />
+        <label className='profile__label' htmlFor='email'>
+          E-mail
+        </label>
+        <input
+          className='profile__field'
+          type='email'
+          name='email'
+          id='email'
+          placeholder={user.email}
+          required
+        />
+      </form>
+      <button className='profile__button'>Редактировать</button>
+      <button className='profile__button profile__button_red'>Выйти из аккаунта</button>
+    </div>
+  )
+}
+
+export default Profile
