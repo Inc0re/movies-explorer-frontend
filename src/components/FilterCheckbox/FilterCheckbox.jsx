b041335db076144617c8bafa8d221c1331ab8c8f
Daniil Borovov <superskillbro@gmail.com>
2023-07-27 03:48:14 +0600

    add adaptive for 320px

--- src/components/FilterCheckbox/FilterCheckbox.jsx
+++ src/components/FilterCheckbox/FilterCheckbox.jsx
@@ -2,12 +2,13 @@ import './FilterCheckbox.css'
 
 function FilterCheckbox({ label, id }) {
   return (
-    <>
+    <div className='filter-checkbox'>
       <input className='filter-checkbox__input' type='checkbox' id={id} />
-      <label className='filter-checkbox__label' htmlFor={id}>
+      <label className='filter-checkbox__label' htmlFor={id} />
+      <label className='filter-checkbox__text' htmlFor={id}>
         {label}
       </label>
-    </>
+    </div>
   )
 }
 
