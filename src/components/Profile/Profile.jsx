f2f274d56437eb72735a02633f1b601d6241d766
Daniil Borovov <superskillbro@gmail.com>
2023-07-27 22:19:28 +0600

    add navigate for logOut button

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,13 +1,20 @@
+import { useNavigate } from 'react-router-dom'
 import './Profile.css'
 
 function Profile() {
+
+  const navigate = useNavigate()
+
+  const handleLogout = () => {
+    navigate('/')
+  }
   const user = {
     name: 'Виталий',
     email: 'test@ya.ru',
   }
   return (
     <div className='profile'>
-      <h1 className='profile__title'>Привет, {user.name}</h1>
+      <h1 className='profile__title'>Привет, {user.name}!</h1>
       <form onSubmit={console.log('submit')} className='profile__form'>
         <label className='profile__label' htmlFor='name'>
           Имя
@@ -17,7 +24,8 @@ function Profile() {
           type='text'
           name='name'
           id='name'
-          placeholder={user.name}
+          placeholder='Иван'
+          value={user.name}
           required
         />
         <div className='profile__split' />
@@ -29,12 +37,13 @@ function Profile() {
           type='email'
           name='email'
           id='email'
-          placeholder={user.email}
+          placeholder='example@ya.ru'
+          value={user.email}
           required
         />
       <div className='profile__buttons'>
         <button className='profile__button'>Редактировать</button>
-        <button className='profile__button profile__button_red'>Выйти из аккаунта</button>
+        <button className='profile__button profile__button_red' onClick={handleLogout}>Выйти из аккаунта</button>
       </div>
       </form>
     </div>
