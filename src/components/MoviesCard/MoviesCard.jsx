36247064fd3320fd10fd18cfa6d9d3fddf91c6c8
Daniil Borovov <superskillbro@gmail.com>
2023-08-05 02:29:00 +0600

    add duration calc and onSave function

--- src/components/MoviesCard/MoviesCard.jsx
+++ src/components/MoviesCard/MoviesCard.jsx
@@ -1,12 +1,27 @@
 import './MoviesCard.css'
 
-function MoviesCard({ card, savedClass }) {
+function MoviesCard({ card, savedClass, onSave }) {
   const { nameRU, duration, image, isSaved} = card
+
+  function handleCardSave() {
+    onSave(card)
+  }
+
+  function getDurationText() {
+    if (duration < 60) {
+      return `${duration} минут`
+    } else {
+      const hours = Math.floor(duration / 60)
+      const minutes = duration % 60
+      return `${hours}ч ${minutes}м`
+    }
+  }
+
   return (
     <li className='movie-card'>
       <div className='movie-card__header'>
         <h2 className='movie-card__title'>{nameRU}</h2>
-        <p className='movie-card__length'>{`${duration} минут`}</p>
+        <p className='movie-card__length'>{getDurationText()}</p>
       </div>
       <img
         src={'https://api.nomoreparties.co' + image.url}
@@ -16,6 +31,7 @@ function MoviesCard({ card, savedClass }) {
       <button
         className={`movie-card__button${isSaved ? ` ${savedClass}` : ''}`}
         type='button'
+        onClick={handleCardSave}
       >
         {!isSaved && 'Сохранить'}
       </button>
