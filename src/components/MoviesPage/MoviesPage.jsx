286336b5850a67319b5bd23c5afe14b683a2186a
Daniil Borovov <borovovdaniil@gmail.com>
2023-08-05 23:38:26 +0600

    refactor functions and move some to separate file

--- src/components/MoviesPage/MoviesPage.jsx
+++ src/components/MoviesPage/MoviesPage.jsx
@@ -4,7 +4,7 @@ import Header from '../Header/Header'
 import Movies from '../Movies/Movies'
 import moviesApi from '../../utils/MoviesApi'
 import mainApi from '../../utils/MainApi'
-import { filterByDuration, filterByTitle } from '../../utils/moviesFilters'
+import { searchMovies, loadMore } from '../../utils/moviesFilters'
 
 function MoviesPage({ loggedIn, isSaved }) {
   // стейты для /movies
@@ -62,110 +62,142 @@ function MoviesPage({ loggedIn, isSaved }) {
       })
   }, [])
 
-  // при переключении тумблера вызываем поиск
+  // при переключении тумблера фильтруем уже найденное по времени
+  useEffect(() => {
+    if (!currentState === 'loaded' || searchQuery === '') return
+    searchMovies(
+      setCurrentState,
+      setPageText,
+      setFilteredMovies,
+      setDisplayedMovies,
+      movies,
+      searchQuery,
+      tumblerState
+    )
+  }, [tumblerState])
+
+  useEffect(() => {
+    if (!currentState === 'loaded' || searchQuery === '') return
+    searchMovies()
+  }, [savedTumblerState])
+
+  // при изменении одного из стейтов хранимых в localStorage, обновляем localStorage
   useEffect(() => {
     if (currentState === 'loaded') {
-      searchMovies()
+      setLocalStorage()
     }
-  }, [tumblerState])
+  }, [filteredMovies, displayedMovies, searchQuery, tumblerState])
+
+  // при изменении массива сохраненных фильмов проверяем какие из фильмов есть в массиве всех фильмов и меняем у них флаг isSaved
+  useEffect(() => {
+    if (savedCurrentState === 'loaded') {
+      const newMovies = movies.map(movie => {
+        if (savedMovies.find(savedMovie => savedMovie.movieId === movie.id)) {
+          return { ...movie, isSaved: true }
+        } else {
+          return { ...movie, isSaved: false }
+        }
+      })
+      setMovies(newMovies)
+    }
+  }, [savedMovies])
 
   function handleSearch(e) {
     e.preventDefault()
-    searchMovies()
+    searchMovies(
+      setCurrentState,
+      setPageText,
+      setFilteredMovies,
+      setDisplayedMovies,
+      movies,
+      searchQuery,
+      tumblerState
+    )
+  }
+
+  function handleSavedSearch(e) {
+    e.preventDefault()
+    searchMovies(
+      setSavedCurrentState,
+      setSavedPageText,
+      setSavedFilteredMovies,
+      null,
+      savedMovies,
+      savedSearchQuery,
+      savedTumblerState
+    )
   }
 
   function handleTumblerSwitch() {
     setTumblerState(!tumblerState)
   }
 
+  function handleSavedTumblerSwitch() {
+    setSavedTumblerState(!savedTumblerState)
+  }
+
   function handleSearchQueryChange(e) {
     setSearchQuery(e.target.value)
   }
 
-  function calcInitialMovies() {
-    const windowWidth = window.innerWidth
-    if (windowWidth > 1074) {
-      return 12
-    } else if (windowWidth > 682) {
-      return 8
-    } else {
-      return 5
-    }
-  }
-
-  function calcMoviesToLoad() {
-    const windowWidth = window.innerWidth
-    if (windowWidth > 1074) {
-      return 3
-    } else {
-      return 2
-    }
-  }
-
-  function handleLoadMore() {
-    const moviesToLoad = calcMoviesToLoad()
-    const moviesLeft = filteredMovies.length - displayedMovies.length
-    if (moviesLeft > moviesToLoad) {
-      setDisplayedMovies([
-        ...displayedMovies,
-        ...filteredMovies.slice(
-          displayedMovies.length,
-          displayedMovies.length + moviesToLoad
-        ),
-      ])
-    } else {
-      setDisplayedMovies([
-        ...displayedMovies,
-        ...filteredMovies.slice(displayedMovies.length, filteredMovies.length),
-      ])
-    }
+  function handleSavedSearchQueryChange(e) {
+    setSavedSearchQuery(e.target.value)
   }
 
   function handleMovieBtnClick(e) {
-    console.log(e)
+    if (e.isSaved) {
+      handleMovieDelete(e)
+    } else {
+      handleMovieSave(e)
+    }
   }
 
-  function handleMovieSave() {}
+  // TODO: добавить проверку на то, что фильм уже сохранен
+  function handleMovieSave(m) {
+    mainApi
+      .saveMovie({
+        movieId: m.id,
+        nameRU: m.nameRU,
+        nameEN: m.nameEN,
+        duration: m.duration,
+        image: `https://api.nomoreparties.co${m.image.url}`,
+        trailer: m.trailerLink,
+        thumbnail: m.image.formats.thumbnail.url
+          ? `https://api.nomoreparties.co${m.image.formats.thumbnail.url}`
+          : null,
+        country: m.country,
+        director: m.director,
+        year: m.year,
+        description: m.description,
+      })
+      .then(res => {
+        setSavedMovies([...savedMovies, res.data])
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
 
-  function handleMovieDelete() {}
+  function handleMovieDelete(m) {
+    console.log(m)
+  }
 
-  // главная функция поиска фильмов
-  function searchMovies() {
-    // ставим прелоадер
-    setCurrentState('loading')
-    // фильтруем фильмы по имени
-    let filteredMovies = filterByTitle(movies, searchQuery)
+  function setLocalStorage() {
+    localStorage.setItem(
+      'savedSearch',
+      JSON.stringify({
+        currentState: 'loaded',
+        movies,
+        filteredMovies,
+        displayedMovies,
+        searchQuery,
+        tumblerState,
+      })
+    )
+  }
 
-    // если включен тумблер, фильтруем еще и по длительности
-    if (tumblerState) {
-      filteredMovies = filterByDuration(filteredMovies, 40)
-    }
-    console.log('Найдено: ' + filteredMovies.length)
-    // если ничего не найдено, ставим соответствующий текст
-    if (filteredMovies.length === 0) {
-      setPageText('Ничего не найдено')
-      setCurrentState('')
-    } else {
-      // иначе:
-      // 1. добавляем результат в стейт отфильтрованных фильмов
-      // 2. добавляем в стейт отображаемых фильмов нужое количество относительно ширины экрана
-      // 3. меняем стейт на 'loaded'
-      // 4. сохраняем результаты поиска в localStorage
-      setFilteredMovies(filteredMovies)
-      setDisplayedMovies(filteredMovies.slice(0, calcInitialMovies()))
-      setCurrentState('loaded')
-      localStorage.setItem(
-        'savedSearch',
-        JSON.stringify({
-          currentState: 'loaded',
-          movies: movies,
-          filteredMovies,
-          displayedMovies,
-          searchQuery,
-          tumblerState,
-        })
-      )
-    }
+  function handleLoadMore() {
+    loadMore(setDisplayedMovies, displayedMovies, filteredMovies)
   }
 
   return (
@@ -180,9 +212,9 @@ function MoviesPage({ loggedIn, isSaved }) {
           searchQuery={savedSearchQuery}
           tumblerState={savedTumblerState}
           pageText={savedPageText}
-          handleSearch={handleSearch}
-          handleTumblerSwitch={handleTumblerSwitch}
-          handleSearchQueryChange={handleSearchQueryChange}
+          handleSearch={handleSavedSearch}
+          handleTumblerSwitch={handleSavedTumblerSwitch}
+          handleSearchQueryChange={handleSavedSearchQueryChange}
           handleMovieBtnClick={handleMovieBtnClick}
         />
       ) : (
