d9970d1f539138583f0c3ee05af08179d66ecacb
Daniil Borovov <superskillbro@gmail.com>
2023-08-09 08:26:43 +0600

    use nagivate(-1) for back button

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,13 +1,16 @@
 import './NotFound.css'
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 function NotFound() {
+  const navigate = useNavigate()
   return (
     <main>
       <section className='not-found'>
         <h1 className='not-found__code'>404</h1>
         <p className='not-found__text'>Страница не найдена</p>
-        <Link className='not-found__link' to='/'>Назад</Link>
+        <button className='not-found__link' onClick={() => navigate(-1)}>
+          Назад
+        </button>
       </section>
     </main>
   )
